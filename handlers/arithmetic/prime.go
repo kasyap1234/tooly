6accca7d94833c020f54ff7fc224451f1342f0b8
kasyap dharanikota <76957823+kasyap1234@users.noreply.github.com>
2024-06-20 00:12:10 +0530

    changes in mac

--- handlers/arithmetic/prime.go
+++ handlers/arithmetic/prime.go
@@ -1,3 +1,30 @@
 package arithmetic 
 
 
+import (
+	"encoding/json"
+	"net/http"
+	"github.com/kasyap1234/alltools/handlers/arithmetic/model"
+)
+func IsPrime(n int) bool {
+
+	for i := 2; i < n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+func PrimeHandler(w http.ResponseWriter , r *http.Request)bool {
+	var n int 
+	
+	if err :=json.NewDecoder(r.Body).Decode(&n); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return false; 
+	}
+	return IsPrime(n)
+}
+
+
+
+
