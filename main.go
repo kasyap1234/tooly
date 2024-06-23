805844094df9e4aeefb606fd39b61c0e9a0ebf35
kasyap dharanikota <76957823+kasyap1234@users.noreply.github.com>
2024-06-23 14:00:04 +0530

    main.go commit

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main 
+import (
+	"fmt"
+	"net/http"
+	 "github.com/go-chi/chi"
+	
+)
+func main(){
+	 
+	r :=chi.NewRouter(); 
+	r.Get("/",func (w http.ResponseWriter , r *http.Request){
+		w.Write([]byte("hello world"))
+	})
+	http.ListenAndServe(":3000", r)
+}
+
