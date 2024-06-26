8fb5aee692d29fc67ba51b20744ce8614b9a4ad6
kasyap dharanikota <76957823+kasyap1234@users.noreply.github.com>
2024-06-27 02:40:04 +0530

    feature: image feature started

--- handlers/image/image.go
+++ handlers/image/image.go
@@ -6,6 +6,16 @@ import (
 )
 
 func GenerateImageAI(input string)string {
-	
 
+return input ; 
+
+}
+func ImageHandler(w http.ResponseWriter , r *http.Request){
+	var input string 
+
+	if err :=json.NewDecoder(r.Body).Decode(&input); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return 
+	}
+	w.Write([]byte(GenerateImageAI(input)))
 }
